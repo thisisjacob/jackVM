dd203025fe60feb99c1ab01500899d083f61d73b
thisisjacob <jacobholfelder@gmail.com>
2020-08-04 17:01:40 -0400

    Fixed eq not saving result to correct memory address

--- VirtualMachine/CodeWriter.cpp
+++ VirtualMachine/CodeWriter.cpp
@@ -13,21 +13,21 @@ void CodeWriter::writeArithmetic(const std::string& command, const int currentAd
 	// currentAddress is the next available empty address - use these to point to items
 	int firstAddress = currentAddress - 2;
 	int secondAddress = currentAddress - 1;
-	if (command.compare("add") == 0) {
+	if (command == "add") {
 		outFileStream << "A=A-1" << "\n";
 		outFileStream << "D=M+D" << "\n";
 		outFileStream << "M=D" << "\n";
 	}
-	else if (command.compare("sub") == 0) {
+	else if (command == "sub") {
 		outFileStream << "A=A-1" << "\n";
 		outFileStream << "D=M-D" << "\n";
 		outFileStream << "M=D" << "\n";
 	}
-	else if (command.compare("neg") == 0) {
+	else if (command == "neg") {
 		outFileStream << "D=-M" << "\n";
 		outFileStream << "M=D" << "\n";
 	}
-	else if (command.compare("eq") == 0) {
+	else if (command == "eq") {
 		// checking for equality
 		outFileStream << "@" << firstAddress << "\n";
 		outFileStream << "D=M\n";
@@ -39,20 +39,47 @@ void CodeWriter::writeArithmetic(const std::string& command, const int currentAd
 		outFileStream << "0;JMP\n";
 		// equal return value
 		outFileStream << "\t(ISEQ" << std::to_string(uniqueSymbolCounter) << ")\n";
-		outFileStream << "@" << firstAddress << "\n";
+		outFileStream << "@" << secondAddress << "\n";
 		outFileStream << "M=-1\n";
 		outFileStream << "@END" << std::to_string(uniqueSymbolCounter) << "\n";
 		outFileStream << "0;JMP\n";
 		// not equal return value
 		outFileStream << "\t(NOTEQ" << std::to_string(uniqueSymbolCounter) << ")\n";
+		outFileStream << "@" << secondAddress << "\n";
+		outFileStream << "M=0\n";
+		outFileStream << "@END" << std::to_string(uniqueSymbolCounter) << "\n";
+		outFileStream << "0;JMP\n";
+		// end of equality block
+		outFileStream << "\t(END" << std::to_string(uniqueSymbolCounter) << ")\n";
+	}
+	else if (command == "gt") {
+		// checking if firstAddress is greater than secondAddress
+		// TODO: these first for lines for logical operators could be moved into a separate function for code reuse
+		// TODO: return values of comparisons could also be moved into a function for code reuse
+		outFileStream << "@" << firstAddress << "\n";
+		outFileStream << "D=M\n";
+		outFileStream << "@" << secondAddress << "\n";
+		outFileStream << "D=D-M\n";
+		outFileStream << "@ISGREATER" << std::to_string(uniqueSymbolCounter) << "\n";
+		outFileStream << "D;JGT\n";
+		outFileStream << "@NOTGREATER" << std::to_string(uniqueSymbolCounter) << "\n";
+		outFileStream << "0;JMP\n";
+		// greater than return value
+		outFileStream << "\t(ISGREATER" << std::to_string(uniqueSymbolCounter) << ")\n";
+		outFileStream << "@" << firstAddress << "\n";
+		outFileStream << "M=-1\n";
+		outFileStream << "@END" << std::to_string(uniqueSymbolCounter) << "\n";
+		outFileStream << "0;JMP\n";
+		// not greater than return value
+		outFileStream << "\t(NOTGREATER" << std::to_string(uniqueSymbolCounter) << ")\n";
 		outFileStream << "@" << firstAddress << "\n";
 		outFileStream << "M=0\n";
 		outFileStream << "@END" << std::to_string(uniqueSymbolCounter) << "\n";
 		outFileStream << "0;JMP\n";
 		// end of equality block
 		outFileStream << "\t(END" << std::to_string(uniqueSymbolCounter) << ")\n";
-		uniqueSymbolCounter++;
 	}
+	uniqueSymbolCounter++;
 }
 
 void CodeWriter::writePushPop(const VMCommandType command, const std::string& segment, const int index) {
